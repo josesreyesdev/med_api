4fe3b02ac5b0833ffd4848f5e3c48c2ded2a0a24
Jose SR <josesanchez09190@gmail.com>
2024-11-17 23:15:21 -0600

    Added project

---
+++ src/main/kotlin/com/jsrdev/med_api/MedApiApplication.kt
@@ -0,0 +1,11 @@
+package com.jsrdev.med_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MedApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<MedApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/jsrdev/med_api/MedApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jsrdev.med_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MedApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
