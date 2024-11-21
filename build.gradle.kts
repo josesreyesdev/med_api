803b5336d07f3bd11ffb1f822b376c42ae833b5b
Jose SR <josesanchez09190@gmail.com>
2024-11-20 22:14:23 -0600

    added dependencies and config database

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
     id("io.spring.dependency-management") version "1.1.6"
 
     kotlin("plugin.serialization") version "1.6.0"
+
+    kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "com.jsrdev"
@@ -29,6 +31,16 @@ dependencies {
     // serialize and deserialize json
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
+    // mysql
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // flyway
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -38,6 +50,12 @@ kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
     }
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
 }
 
 tasks.withType<Test> {
