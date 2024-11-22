3ec33f158beb51d20b65dea1707e0300bf63d16c
Jose SR <josesanchez09190@gmail.com>
2024-11-21 23:14:18 -0600

    added migration V1 to create table phycians

---
+++ src/main/resources/db/migration/V1__create-table-physicians.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS physicians(
+    id bigint not null auto_increment,
+    name varchar(100) not null,
+    avatar varchar(300) not null,
+    email varchar(100) not null unique,
+    document varchar(9) not null unique,
+    phone_number varchar (15) not null unique,
+    specialty varchar(100) not null,
+    street varchar(200) not null,
+    state_or_province varchar(150) not null,
+    municipality_or_delegation varchar(150) not null,
+    city varchar(100) not null,
+    zip_code varchar(10) not null,
+    country varchar(100) not null,
+    external_number varchar(20) not null,
+    internal_number varchar(20),
+    complement varchar(200),
+
+    primary key(id)
+);
