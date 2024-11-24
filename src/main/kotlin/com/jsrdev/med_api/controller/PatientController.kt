6d5109c9d7c30e7e247cde9e5309065f32bb8bbf
Jose SR <josesanchez09190@gmail.com>
2024-11-24 04:09:27 -0600

    added pagination response to patients

--- src/main/kotlin/com/jsrdev/med_api/controller/PatientController.kt
+++ src/main/kotlin/com/jsrdev/med_api/controller/PatientController.kt
@@ -1,28 +1,39 @@
 package com.jsrdev.med_api.controller
 
-import com.jsrdev.med_api.patient.PatientRequest
 import com.jsrdev.med_api.patient.Patient
 import com.jsrdev.med_api.patient.PatientMapper.toResponse
+import com.jsrdev.med_api.patient.PatientRepository
+import com.jsrdev.med_api.patient.PatientRequest
 import com.jsrdev.med_api.patient.PatientResponse
-import com.jsrdev.med_api.patient.PatientRespository
 import jakarta.transaction.Transactional
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PagedResourcesAssembler
+import org.springframework.hateoas.EntityModel
+import org.springframework.hateoas.PagedModel
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/patients")
 class PatientController @Autowired constructor(
-    private val patientRepository: PatientRespository
+    private val patientRepository: PatientRepository
 ) {
     @PostMapping
     @Transactional
-    fun createPatient(@RequestBody @Valid patientRequest: PatientRequest)  {
+    fun createPatient(@RequestBody @Valid patientRequest: PatientRequest) {
         patientRepository.save(Patient(patientRequest))
     }
 
     @GetMapping
-    fun getPatients(): List<PatientResponse> =
-        patientRepository.findAll()
+    fun getPatients(
+        pagination: Pageable,
+        assembler: PagedResourcesAssembler<PatientResponse>
+    ): PagedModel<EntityModel<PatientResponse>> {
+
+        val patientsPage = patientRepository.findAll(pagination)
             .map { it.toResponse() }
+
+        return assembler.toModel(patientsPage)
+    }
 }
--- src/main/kotlin/com/jsrdev/med_api/patient/PatientRepository.kt
+++ src/main/kotlin/com/jsrdev/med_api/patient/PatientRepository.kt
@@ -2,4 +2,4 @@ package com.jsrdev.med_api.patient
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PatientRespository : JpaRepository<Patient, Long>
+interface PatientRepository : JpaRepository<Patient, Long>
