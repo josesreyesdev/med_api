e5b24b9c1594955af1fbee36dd1d2f8cc4a50ac4
Jose SR <josesanchez09190@gmail.com>
2024-12-11 00:36:41 -0600

    added jwt dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
 
     // jwt
-    implementation("com.auth0:java-jwt:4.4.0")
+    implementation("com.auth0:java-jwt:4.4.0") //4.2.0
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
