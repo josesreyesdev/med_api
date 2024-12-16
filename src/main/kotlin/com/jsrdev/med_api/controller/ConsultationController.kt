2fccaf2438b5f41cd14d68f63fbb7f585c79d5cb
Jose SR <josesanchez09190@gmail.com>
2024-12-15 23:48:45 -0600

    added consultation controller

---
+++ src/main/kotlin/com/jsrdev/med_api/controller/ConsultationController.kt
@@ -0,0 +1,11 @@
+package com.jsrdev.med_api.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/consultation")
+class ConsultationController {
+
+
+}
