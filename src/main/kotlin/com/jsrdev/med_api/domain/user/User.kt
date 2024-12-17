8c71cec189c403ab28acbb6005a86208bdad0c94
Jose SR <josesanchez09190@gmail.com>
2024-12-17 00:00:00 -0600

    added example of implementation of different roles per user

--- src/main/kotlin/com/jsrdev/med_api/domain/user/User.kt
+++ src/main/kotlin/com/jsrdev/med_api/domain/user/User.kt
@@ -15,9 +15,14 @@ data class User(
     @Enumerated(EnumType.STRING)
     val role: Role
 ): UserDetails {
-    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
-        return mutableListOf(SimpleGrantedAuthority("ROLE_${role.name}"))
+    override fun getAuthorities(): Collection<GrantedAuthority> {
+        return listOf(SimpleGrantedAuthority("ROLE_${role.name}"))
     }
+
+    /*in constructor => val roles: List<Role>
+    fun getAuthorities(): Collection<GrantedAuthority> {
+        return roles.map { role -> SimpleGrantedAuthority(role.name) }
+    } */
 
     override fun getPassword(): String = this.pass
 
