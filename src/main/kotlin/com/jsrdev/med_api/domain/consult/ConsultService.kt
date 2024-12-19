f81045ee4f64145357e53110414a7efc049b5cbb
Jose SR <josesanchez09190@gmail.com>
2024-12-19 01:00:33 -0600

    added integrity validation

--- src/main/kotlin/com/jsrdev/med_api/domain/consult/ConsultService.kt
+++ src/main/kotlin/com/jsrdev/med_api/domain/consult/ConsultService.kt
@@ -1,7 +1,9 @@
 package com.jsrdev.med_api.domain.consult
 
 import com.jsrdev.med_api.domain.patient.PatientRepository
+import com.jsrdev.med_api.domain.physician.Physician
 import com.jsrdev.med_api.domain.physician.PhysicianRepository
+import com.jsrdev.med_api.infra.exceptions.ValidateException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
@@ -14,18 +16,26 @@ class ConsultService(
 
     fun addConsult(data: ConsultRequest) {
 
-        val physician = physicianRepository.findByIdOrNull(data.idPhysician)
-            ?: throw IllegalArgumentException("Physician with id: ${data.idPhysician} not found")
+        var physician: Physician? = null
+        data.idPhysician?.let {
+            physician = physicianRepository.findByIdOrNull(data.idPhysician)
+                ?: throw ValidateException("Physician id: ${data.idPhysician} not found")
+        }
+
         val patient = patientRepository.findByIdOrNull(data.idPatient)
-            ?: throw IllegalArgumentException("Patient with id: ${data.idPatient} not found")
+            ?: throw IllegalArgumentException("Patient id: ${data.idPatient} not found")
 
         val consult = Consult(
             id = null,
-            physicianId = physician,
+            physicianId = physician ?: chooseAPhysician(data),
             patientId = patient,
             date = data.date
         )
 
         consultRepository.save(consult)
+    }
+
+    private fun chooseAPhysician(data: ConsultRequest): Physician {
+        TODO("Not yet implemented")
     }
 }
---
+++ src/main/kotlin/com/jsrdev/med_api/infra/exceptions/ValidateException.kt
@@ -0,0 +1,3 @@
+package com.jsrdev.med_api.infra.exceptions
+
+class ValidateException(message: String) : RuntimeException(message)
